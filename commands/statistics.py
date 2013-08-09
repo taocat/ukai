d48f36ff263de3cbcee0f46e9395dd4c58fcd229
Keiichi SHIMA <keiichi@iijlab.net>
2013-08-09 17:46:18 +0900

    Added disk I/O stat structure.

---
+++ commands/statistics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import xmlrpclib
+
+from ukai_config import UKAIConfig
+
+if len(sys.argv) != 2:
+    print 'Usage: %s IMAGE_NAME' % sys.argv[0]
+    exit (-1)
+
+image_name = sys.argv[1]
+
+c = xmlrpclib.ServerProxy('http://127.0.0.1:%d' % UKAIConfig['control_port'])
+print c.get_statistics(image_name)
