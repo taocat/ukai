7a2f8e5ed8bf9ce7c9f72d6385f666a6a8fc79a0
Keiichi SHIMA <keiichi@iijlab.net>
2013-08-14 12:57:13 +0900

    added license text and doc strings.

--- ukai/ukai_utils.py
+++ ukai/ukai_utils.py
@@ -1,3 +1,30 @@
+# Copyright 2013 IIJ Innovation Institute Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY IIJ INNOVATION INSTITUTE INC. ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL IIJ INNOVATION INSTITUTE INC. OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+The ukai_utils.py module provides a set of general functions to other
+UKAI modules.
+'''
+
 import netifaces
 import time
 
@@ -10,6 +37,11 @@ UKAIIfaddrCache = {
 UKAI_IFADDR_CACHE_VALID_TIME = 1
 
 def UKAIIsLocalNode(node):
+    '''
+    The UKAIIsLocalNode function checks if the node is a local node or
+    not by comparing the passed value and all the addresses assigned
+    to local network interfaces.
+    '''
     now = time.time()
 
     if (('ifaddr_cache' in UKAIConfig)
